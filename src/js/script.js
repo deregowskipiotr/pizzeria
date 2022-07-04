ade15e2aefba698a4d6d5faf094e56be66e9ba54
Piotr Deregowski <deregowskipiotr@interia.pl>
2022-07-04 09:12:00 +0200

    Add /modified processOrder method

--- src/js/script.js
+++ src/js/script.js
@@ -94,6 +94,7 @@
       thisProduct.formInputs = thisProduct.form.querySelectorAll(select.all.formInputs);
       thisProduct.cartButton = thisProduct.element.querySelector(select.menuProduct.cartButton);
       thisProduct.priceElem = thisProduct.element.querySelector(select.menuProduct.priceElem);
+      thisProduct.imageWrapper = thisProduct.element.querySelector(select.menuProduct.imageWrapper);
       
     }
 
@@ -165,7 +166,8 @@
           console.log(optionId, option);
           
           // check if there is param with a name of paramId in formData and if it includes optionId
-          if(formData[paramId] && formData[paramId].includes(optionId)){
+          const optionSelected = formData[paramId] && formData[paramId].includes(optionId);
+          if(optionSelected) {
             // check if the option is not default
             if(!option.default == true){
             // add option price to price variable
@@ -176,6 +178,15 @@
             if(option.default == true){
               // reduce price variable
               price = price - option.price;
+            }
+          }
+          // find img
+          const optionImage = thisProduct.imageWrapper.querySelector('.' + paramId + '-' + optionId);
+          if(optionImage) {
+            if(optionSelected) {
+              optionImage.classList.add(classNames.menuProduct.imageVisible);
+            } else {
+              optionImage.classList.remove(classNames.menuProduct.imageVisible);
             }
           }
         }
